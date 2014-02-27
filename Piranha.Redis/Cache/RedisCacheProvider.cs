bca306b6dd64df875afae51c0dfec1ef2401c06f
Håkan Edling <hakan@tidyui.com>
2014-02-27 10:46:59 +0100

    Initial commit.

---
+++ Piranha.Redis/Cache/RedisCacheProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Piranha.Cache
+{
+	public class RedisCacheProvider : ICacheProvider
+	{
+		public object this[string key] {
+			get {
+				throw new NotImplementedException();
+			}
+			set {
+				throw new NotImplementedException();
+			}
+		}
+
+		public void Remove(string key) {
+			throw new NotImplementedException();
+		}
+
+		public bool Contains(string key) {
+			throw new NotImplementedException();
+		}
+	}
+}
